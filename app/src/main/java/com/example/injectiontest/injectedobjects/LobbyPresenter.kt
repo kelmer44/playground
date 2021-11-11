02fd12b616a72e55b9f60f5b372ead001b91c2a2
Gabriel Sanmartín <gabriel.sanmartin@disneystreaming.com>
2021-11-11 15:01:11 +0100

    improved ui separation

--- app/src/main/java/com/example/injectiontest/injectedobjects/LobbyPresenter.kt
+++ app/src/main/java/com/example/injectiontest/injectedobjects/LobbyPresenter.kt
@@ -43,19 +43,18 @@ class LobbyPresenter @Inject constructor(
     }
 
     fun doSomething() {
-        binding.hello.text = "Hello from $this"
-        binding.button.setOnClickListener {
-            viewModel.useHelper()
-        }
-        binding.buttonRouter.setOnClickListener {
-            viewModel.triggerContentRouter()
-        }
+//        binding.hello.text = "Hello from $this"
+//        binding.button.setOnClickListener {
+//            viewModel.useHelper()
+//        }
+//        binding.buttonRouter.setOnClickListener {
+//            viewModel.triggerContentRouter()
+//        }
+//
+//        binding.input.setOnFocusChangeListener { v, hasFocus ->
+//            Timber.i("Focus change on disneyinput $hasFocus (view is $v)")
+//        }
 
-        binding.input.setOnFocusChangeListener { v, hasFocus ->
-            Timber.i("Focus change on disneyinput $hasFocus (view is $v)")
-        }
-
-        val flow = binding.profilesFlowHelper
 
         val lastElement = ColorBox(context = fragment.requireContext()).also {
             it.setBgColor(Color.DKGRAY)
@@ -64,54 +63,53 @@ class LobbyPresenter @Inject constructor(
         }
         val dimension = fragment.resources.getDimension(R.dimen.colorview_width)
         val horizontalGap = fragment.resources.getDimension(R.dimen.flow_horizontal_gap)
-        binding.flowRoot.doOnLayout {
-            val maxWidth = binding.flowRoot.width
-
-            val combinedWidth = dimension + horizontalGap
-            var remainingWidth = maxWidth - dimension
-            var items = 1
-            while (remainingWidth > (combinedWidth)) {
-                remainingWidth -= (combinedWidth)
-                items++
-            }
-            Timber.w("Items per row are $items")
-
-
-            val views = listOf(
-                ColorBox(context = fragment.requireContext()).also {
-                    it.setBgColor(Color.MAGENTA)
-                    it.setText("Magenta Color")
-                    it.id = View.generateViewId()
-                },
-                ColorBox(context = fragment.requireContext()).also {
-                    it.setBgColor(Color.BLUE)
-                    it.setText("Blue Color")
-                    it.id = View.generateViewId()
-                },
-                ColorBox(context = fragment.requireContext()).also {
-                    it.setBgColor(Color.GREEN)
-                    it.setText("Green Color")
-                    it.id = View.generateViewId()
-                },
-                ColorBox(context = fragment.requireContext()).also {
-                    it.setBgColor(Color.RED)
-                    it.setText("Red Color")
-                    it.id = View.generateViewId()
-                },
-                ColorBox(context = fragment.requireContext()).also {
-                    it.setBgColor(Color.YELLOW)
-                    it.setText("Yellow Color")
-                    it.id = View.generateViewId()
-                },
-                ColorBox(context = fragment.requireContext()).also {
-                    it.setBgColor(Color.GRAY)
-                    it.setText("Gray Color with long name")
-                    it.id = View.generateViewId()
-                },
-                lastElement,
-            )
-            val previousViews = binding.flowRoot.children.filter { it is Flow }.toList()
-            flow.updateViews(views, emptyList())
-        }
+//        binding.flowRoot.doOnLayout {
+//            val maxWidth = binding.flowRoot.width
+//
+//            val combinedWidth = dimension + horizontalGap
+//            var remainingWidth = maxWidth - dimension
+//            var items = 1
+//            while (remainingWidth > (combinedWidth)) {
+//                remainingWidth -= (combinedWidth)
+//                items++
+//            }
+//            Timber.w("Items per row are $items")
+//
+//
+//            val views = listOf(
+//                ColorBox(context = fragment.requireContext()).also {
+//                    it.setBgColor(Color.MAGENTA)
+//                    it.setText("Magenta Color")
+//                    it.id = View.generateViewId()
+//                },
+//                ColorBox(context = fragment.requireContext()).also {
+//                    it.setBgColor(Color.BLUE)
+//                    it.setText("Blue Color")
+//                    it.id = View.generateViewId()
+//                },
+//                ColorBox(context = fragment.requireContext()).also {
+//                    it.setBgColor(Color.GREEN)
+//                    it.setText("Green Color")
+//                    it.id = View.generateViewId()
+//                },
+//                ColorBox(context = fragment.requireContext()).also {
+//                    it.setBgColor(Color.RED)
+//                    it.setText("Red Color")
+//                    it.id = View.generateViewId()
+//                },
+//                ColorBox(context = fragment.requireContext()).also {
+//                    it.setBgColor(Color.YELLOW)
+//                    it.setText("Yellow Color")
+//                    it.id = View.generateViewId()
+//                },
+//                ColorBox(context = fragment.requireContext()).also {
+//                    it.setBgColor(Color.GRAY)
+//                    it.setText("Gray Color with long name")
+//                    it.id = View.generateViewId()
+//                },
+//                lastElement,
+//            )
+////            flow.updateViews(views, emptyList())
+//        }
     }
 }
